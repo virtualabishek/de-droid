2fd0a36b5aa6f8f84bfe1d65a5329f581af20fd3
virtualabishek <717abishekneupane@gmail.com>
2026-05-24 22:12:35 +0545

    fix: import issue

--- desktop-app/src/renderer/components/PackageList.tsx
+++ desktop-app/src/renderer/components/PackageList.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo, useEffect, useRef } from "react";
+import { useState, useMemo, useEffect, useRef, useCallback } from "react";
 import { useVirtualizer } from "@tanstack/react-virtual";
 import Fuse from "fuse.js";
 import { useLocation } from "react-router-dom";
